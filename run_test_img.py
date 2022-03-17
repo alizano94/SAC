ee3354f84e8f0c0e669bb64cc3a2c6790dc7a79f
Andres <lizanoandresfco@gmail.com>
2022-03-17 18:01:20 -0500

    dum

--- run_test_img.py
+++ run_test_img.py
@@ -2,12 +2,27 @@ import numpy as np
 import matplotlib.pyplot as plt
 from src.control import RL
 
+plot = False
 initial_image = '/home/lizano/Documents/SAC/data/initialstates/Crystal_test.png'
 
+
 control = RL(w=100,m=1,a=4)
-img_batch = control.preProcessImg(initial_image,IMG_H=848,IMG_W=848)
-plt.imshow(img_batch[0])
-plt.gray()
-plt.axis('off')
-plt.show()
-plt.clf()
+control.createAE()
+control.loadAE(None)
+img_batch = control.preProcessImg(initial_image)
+print(img_batch)
+if plot:
+    plt.imshow(img_batch[0])
+    plt.gray()
+    plt.axis('off')
+    plt.show()
+    plt.clf()
+
+reconstructed = 255*control.autoencoder.predict(img_batch)
+print(reconstructed)
+if plot:
+    plt.imshow(reconstructed[0])
+    plt.gray()
+    plt.axis('off')
+    plt.show()
+    plt.clf()
