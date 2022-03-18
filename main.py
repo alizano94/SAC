9b2b9bdcc918867152c4e90cad283b19e1859c6a
Andres Lizano-Villalobos <lizanoandresfco@gmail.com>
2022-03-18 01:52:31 -0500

    ..

--- main.py
+++ main.py
@@ -5,4 +5,6 @@ from src.control import RL
 initial_image = '/home/lizano/Documents/SAC/data/initialstates/Crystal_test.png'
 
 control = RL(w=100,m=1,a=4)
-control.createCNN_DS('UMAP-10D-clusters.csv')
+control.umap(n=100)
+control.cluster_hdbscan(n=100)
+control.createCNN_DS('UMAP-100D-clusters.csv')
