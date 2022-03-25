954b715d61fb268c5da97235294d7284b56d82c3
Andres Lizano-Villalobos <lizanoandresfco@gmail.com>
2022-03-25 08:46:15 -0500

    new clusters

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ from src.control import RL
 initial_image = '/home/lizano/Documents/SAC/data/initialstates/Crystal_test.png'
 
 control = RL(w=100,m=1,a=4)
-control.umap(n=3)
+control.createCNN_DS(file='UMAP-3D-clusters.csv')
