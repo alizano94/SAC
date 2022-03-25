e0bce7111a47461304dd00f9b3467a5b2fd3c0de
Andres <lizanoandresfco@gmail.com>
2022-03-25 08:29:27 -0500

    dum

--- main.py
+++ main.py
@@ -5,6 +5,4 @@ from src.control import RL
 initial_image = '/home/lizano/Documents/SAC/data/initialstates/Crystal_test.png'
 
 control = RL(w=100,m=1,a=4)
-control.umap(n=2)
-control.cluster_hdbscan(n=2)
-control.createCNN_DS('UMAP-2D-clusters.csv')
+control.umap(n=3)
