3f73e64b877d1d1340eb098f766ec24b32390066
Lizano <lizanoandresfco@gmail.com>
2022-05-05 15:37:57 -0500

    dum

--- main.py
+++ main.py
@@ -8,8 +8,4 @@ projected_features_csv = "full_umap_2D_features.csv"
 clusters_csv = 'full_hdbscan_UMAP2.csv'
 
 control = RL(w=100,m=1,a=4)
-control.umap(load_file=full_features_csv,
-            out_file=projected_features_csv,
-            n_components=2,
-            n_neighbors=30,
-            min_dist=0.0)
+control.createCNN_DS(load_file='full_hdbscan_UMAP2.csv')
